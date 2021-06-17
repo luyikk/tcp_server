0f5960df5bce0993f5952ebd8720dda9d4252175
yi lu <luyikk@126.com>
2021-06-17 08:17:33 +0800

    set ssl verify

--- examples/ssl_server.rs
+++ examples/ssl_server.rs
@@ -2,7 +2,7 @@
 use anyhow::*;
 use lazy_static::lazy_static;
 use log::LevelFilter;
-use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod};
+use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod, SslVerifyMode};
 use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Duration;
@@ -12,13 +12,32 @@ use tokio::time::sleep;
 use tokio_openssl::SslStream;
 lazy_static! {
     pub static ref SSL: SslAcceptor = {
-        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls_server()).unwrap();
+        acceptor.set_ca_file("tests/ca-cert.pem").unwrap();
         acceptor
-            .set_private_key_file("tests/key.pem", SslFiletype::PEM)
+            .set_private_key_file("tests/server-key.pem", SslFiletype::PEM)
             .unwrap();
         acceptor
-            .set_certificate_chain_file("tests/cert.pem")
+            .set_certificate_chain_file("tests/server-cert.pem")
             .unwrap();
+        acceptor.set_verify_callback(SslVerifyMode::PEER|SslVerifyMode::FAIL_IF_NO_PEER_CERT,|ok,cert|{
+            println!("pre verify ok {}",ok);
+            if !ok{
+                match cert.verify_cert(){
+                    Ok(v)=>{
+                        println!("verify {}",v)
+                    },
+                    Err(_)=>{
+                        if let Some(cert)= cert.current_cert(){
+                           println!("subject info {:?}",cert.subject_name());
+                           println!("issuer info {:?}",cert.issuer_name());
+                        }
+                    }
+                }
+            }
+            true
+        });
+        acceptor.check_private_key().unwrap();
         acceptor.build()
     };
 }
--- examples/test_ssl_client.rs
+++ examples/test_ssl_client.rs
@@ -1,5 +1,5 @@
 use anyhow::*;
-use openssl::ssl::{SslConnector, SslMethod};
+use openssl::ssl::{SslConnector, SslMethod, SslFiletype};
 use std::pin::Pin;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
@@ -7,11 +7,15 @@ use tokio_openssl::SslStream;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut connector = SslConnector::builder(SslMethod::tls())?;
-    connector.set_ca_file("tests/cert.pem")?;
+    let mut connector = SslConnector::builder(SslMethod::tls_client())?;
+    connector.set_ca_file("tests/server-cert.pem")?;
+    connector.set_private_key_file("tests/client-key.pem", SslFiletype::PEM)?;
+    connector.set_certificate_chain_file("tests/client-cert.pem")?;
+    connector.check_private_key()?;
+
     let ssl = connector.build().configure()?.into_ssl("localhost")?;
 
-    let stream = TcpStream::connect("127.0.0.1:1002").await.unwrap();
+    let stream = TcpStream::connect("127.0.0.1:5555").await.unwrap();
     let mut stream = SslStream::new(ssl, stream).unwrap();
     Pin::new(&mut stream).connect().await.unwrap();
     stream.write_all(b"asdf\r\n").await.unwrap();
